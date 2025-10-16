a60fef9415e573edeff6a454e61aed4ffb9ae96d
gmguerrerocv-byte <gmguerrerocv@gmail.com>
2025-10-16 15:58:10 -0300

    updatepeliculas

--- peliculas.php
+++ peliculas.php
@@ -1,71 +1,56 @@
+<?php
+
+require_once "utils/db_connection.php";
+require_once "utils/funciones.php";
+
+$tabla = $_GET["categoria"] ? $_GET["categoria"] : FALSE;
+
+$tablas= [
+'personajes'=> ['personaje'=>'bienvenidos'],
+'vehiculos'=>['vehiculo'=>'bienvenidos'],
+'tiempo'=>['tiempo'=>'bienvenidos'],
+'cameos'=>['cameo'=>'bienvenidos'],
+'peliculas'=>['pelicula'=>'bienvenidos']
+
+];
+
+if(!array_key_exists($tabla,$tablas)){
+
+header('location: error404.php');
+}
+
+$categorias = listar_todo($conn, $tabla); 
+
+?>
 <?php require "partials/header.php"; ?>
 
 <h1 class="text-center p-5">PELICULAS</h1>
 
 
 
-
+<?php foreach ($categorias as $c) { ?>
 
   <div class="container card mb-3 p-2 ">
     <div class="row">
       <div class="col-2 p-2 ms-5">
-        <img src="img/img-peliculas/pelicula1.jpg" class="img-fluid rounded" alt="Imagen de portada de volver al futuro 1">
+        <img src="img/img-peliculas/<?= $c['img1'] ?>" class="img-fluid rounded" alt="Imagen de portada de volver al futuro 1">
       </div>
 
       <div class="col-9 mx-auto">
         <div class="card-body">
           <div>
-          <h5 class="card-title">Volver al futuro</h5>
+          <h5 class="card-title"><?= $c['nombre'] ?></h5>
           </div>
           <div>
-          <p class="card-text">El adolescente Marty McFly es amigo de Doc, un científico que ha construido una máquina del tiempo. Cuando los dos prueban el artefacto, un error fortuito hace que Marty llegue a 1955, año en el que sus padres iban al instituto y todavía no se habían conocido. Después de impedir su primer encuentro, Marty deberá conseguir que se conozcan y se enamoren, de lo contrario su existencia no sería posible.</p>
+          <p class="card-text"><?= $c['descripcion'] ?></p>
           </div>
           <p class="card-text"><small class="text-body-secondary"></small></p>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="container card mb-3 p-2 ">
-    <div class="row">
-      <div class="col-2 p-2 ms-5">
-        <img src="img/img-peliculas/pelicula2.jpg" class="img-fluid rounded" alt="Imagen de portada de volver al futuro 2">
-      </div>
-
-      <div class="col-9 mx-auto">
-        <div class="card-body">
-          <div>
-          <h5 class="card-title">Volver al futuro 2</h5>
-          </div>
-          <div>
-          <p class="card-text">Doc vuelve a aparecer con una máquina del tiempo mucho más modernizada y le pide a Marty y a su novia que le acompañen al futuro. En el Hill Valley de 2015 deberán solucionar un problema con la ley que tendrá uno de los futuros hijos de Marty y Jenny. En el futuro, Biff Tannen roba la máquina del tiempo y vuelve al pasado para darle a su hijo un libro con estadísticas deportivas para poder ganar una enorme fortuna en las apuestas. Marty y Doc tendrán que parar la posible catástrofe del destino.</p>
-          </div>
-          <p class="card-text"><small class="text-body-secondary"></small></p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="container card mb-3 p-2 ">
-    <div class="row">
-      <div class="col-2 p-2 ms-5">
-        <img src="img/img-peliculas/pelicula3.jpg" class="img-fluid rounded" alt="Imagen de portada de volver al futuro 3">
-      </div>
-
-      <div class="col-9 mx-auto">
-        <div class="card-body">
-          <div>
-          <h5 class="card-title">Volver al futuro 3</h5>
-          </div>
-          <div>
-          <p class="card-text">Marty McFly sigue en 1955 y Doc ha retrocedido al año 1885, la época del salvaje oeste. Marty recibe una carta de Doc en la que le informa de que la máquina del tiempo está averiada y no puede volver al presente, pero que no le importa seguir en el pasado. Sin embargo, Marty descubre una tumba en la que lee que Doc murió en 1885 y decide ir a rescatar a su amigo.</p>
-          </div>
-          <p class="card-text"><small class="text-body-secondary"></small></p>
-        </div>
-      </div>
-    </div>
-  </div>
-
+<?php } ?>
+   
 
 
 
